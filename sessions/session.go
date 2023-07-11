167238236107a33ddae839ce5c97a873f23b859d
aashish2057 <aashish2057@gmail.com>
2023-07-11 16:22:23 -0700

    final changes

--- sessions/session.go
+++ sessions/session.go
@@ -1,11 +1,34 @@
 package sessions
 
 import (
-	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/aashish2057/plexgoutils/server"
 )
 
-func GetActiveSessions(s server.Server) {
-	fmt.Println("Url: " + s.url + " Token: " + s.token)
+func GetActiveSessions(s server.Server) (string, error) {
+	url := s.GetUrl() + "/status/sessions"
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("X-Plex-Token", s.GetToken())
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
 }
