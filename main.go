7aad0b139aa30dab23dbd977b5348fdbd719f79c
aashish2057 <aashish2057@gmail.com>
2023-07-11 16:22:23 -0700

    final changes

--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 	"github.com/joho/godotenv"
 
 	"github.com/aashish2057/plexgoutils/server"
+	"github.com/aashish2057/plexgoutils/sessions"
 )
 
 func main() {
 	godotenv.Load()
-	var s server.Server
-	s.SetUrl(os.Getenv("PLEX_URL"))
-	s.SetToken(os.Getenv("PLEX_TOKEN"))
+	var server1 server.Server
+	server1.SetUrl(os.Getenv("PLEX_URL"))
+	server1.SetToken(os.Getenv("PLEX_TOKEN"))
+
+	res, err := sessions.GetActiveSessions(server1)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
 }
